5db1590072335200d614dc589dc7651b20a7ea8c
P-Pavan <ppavan2002@gmail.com>
2022-10-03 21:19:28 +0530

    oct

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let buttons = document.querySelector('.buttons');
 let btn = document.querySelectorAll('span');
 let value = document.getElementById('value');
-
+let toggleBtn = document.querySelector('.toggleBtn');
+let body = document.querySelector('body');
 for(let i=0; i<btn.length;i++) {
     btn[i].addEventListener("click", function(){
 
